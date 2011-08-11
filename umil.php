044746274cb6e88db3f101ac982552a0ea6f8d39
Nelsaidi <nelsaidi@gmail.com>
2011-08-11 17:24:29 +0100

    Added UMIL

---
+++ umil.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ *
+ * @author Nelsaidi 
+ * @version $Id$
+ * @copyright (c) 2011 Nelsaidi
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+/**
+ * @ignore
+ */
+define('UMIL_AUTO', true);
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+
+include($phpbb_root_path . 'common.' . $phpEx);
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+
+if (!file_exists($phpbb_root_path . 'umil/umil_auto.' . $phpEx))
+{
+	trigger_error('Please download the latest UMIL (Unified MOD Install Library) from: <a href="http://www.phpbb.com/mods/umil/">phpBB.com/mods/umil</a>', E_USER_ERROR);
+}
+
+// The name of the mod to be displayed during installation.
+$mod_name = 'Infractions';
+
+/*
+* The name of the config variable which will hold the currently installed version
+* UMIL will handle checking, setting, and updating the version itself.
+*/
+$version_config_name = 'infractions_version';
+
+
+// The language file which will be included when installing
+$language_file = 'infractions.php';
+
+
+/*
+* Optionally we may specify our own logo image to show in the upper corner instead of the default logo.
+* $phpbb_root_path will get prepended to the path specified
+* Image height should be 50px to prevent cut-off or stretching.
+*/
+//$logo_img = 'styles/prosilver/imageset/site_logo.gif';
+
+/*
+* The array of versions and actions within each.
+* You do not need to order it a specific way (it will be sorted automatically), however, you must enter every version, even if no actions are done for it.
+*
+* You must use correct version numbering.  Unless you know exactly what you can use, only use X.X.X (replacing X with an integer).
+* The version numbering must otherwise be compatible with the version_compare function - http://php.net/manual/en/function.version-compare.php
+*/
+$versions = array(
+	'0.3.0' => array(
+
+		'permission_add' => array(
+			array('m_infractions_issue', 1),
+			array('m_infractions', 1),
+		),
+
+		'permission_set' => array(
+			array('ROLE_MOD_STANDARD', 'm_infractions_issue'),
+			array('ROLE_MOD_STANDARD', 'm_infractions'),
+			
+			array('ROLE_MOD_FULL', 'm_infractions_issue'),
+			array('ROLE_MOD_FULL', 'm_infractions'),
+		),
+
+		'table_add' => array(
+			array('phpbb_infractions', array(
+				'COLUMNS' => array(
+					'infraction_id' 	=> array('INT:11', 0, 'auto_increment'),
+					'type' 			=> array('BOOL', 0),
+					'void'			=> array('BOOL', 0),
+					'user_id' 		=> array('INT:11', 0),
+					'post_id' 		=> array('INT:11', 0),
+					'issuer_id' 		=> array('INT:11', 0),
+					'infraction_points' => array('INT:11', 0),
+					'issue_time' 		=> array('INT:11', 0),
+					'expire_time' 		=> array('INT:11', 0),
+					'duration' 		=> array('INT:11', 0),
+					'reason' 			=> array('TEXT', ''),
+					'banned' 			=> array('BOOL', 0),
+					'ban_id' 			=> array('INT:11', 0),
+					'ban_duration' 	=> array('INT:11', 0),
+					'groups'			=> array('TEXT', ''),
+				),
+
+				'PRIMARY_KEY'	=> array('infraction_id', ''),
+
+				'KEYS'		=> array(
+					'expire_time' 	=> array('INDEX', array('expire_time')),
+					'user_id'		=> array('INDEX', array('user_id')),
+				),
+			)),
+
+			array('phpbb_infraction_templates', array(
+				'COLUMNS' => array(
+					'template_id'		=> array('INT:11', 0, 'auto_increment'),
+					'position' 		=> array('USINT', 0),
+					'name' 			=> array('VCHAR', ''),
+					'reason' 			=> array('TEXT', ''),
+					'infraction_points' => array('INT:11', 0),
+					'duration' 		=> array('INT:11', 0),
+				),
+
+				'PRIMARY_KEY'	=> array('template_id', ''),
+
+				'KEYS'		=> array(
+					'position' => array('INDEX', array('position')),
+				),
+			)),
+
+		),
+
+		'table_column_add' => array(
+			array('USERS_TABLE', 'infraction_points', array('TINT:11', '0')),
+		),
+
+		'config_add' => array(
+			array('infractions', '1', 0),
+		),
+
+		'module_add' => array(
+			array('mcp_infractions', 'mcp',
+				array('module_basename'	=> 0),
+			),
+			
+			array('acp_infractions', 'acp',
+				array('module_basename'	=> 'ACP_CAT_USERGROUP'),
+		),
+
+	),
+);
+
+// Include the UMIL Auto file, it handles the rest
+include($phpbb_root_path . 'umil/umil_auto.' . $phpEx);
