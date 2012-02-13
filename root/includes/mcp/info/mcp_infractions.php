f0a1a195e960c6517ff121741e81c75cd773bf1e
unknown <Nour 2@.(none)>
2012-02-13 20:45:33 +0000

    licensing, some language fixes

--- root/includes/mcp/info/mcp_infractions.php
+++ root/includes/mcp/info/mcp_infractions.php
@@ -1,11 +1,22 @@
 <?php
-
 /**
 * phpBB Infraction System
-* 
-* @package phpBB3
 * @copyright (c) 2012 Nelsaidi
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 *
 */
 
@@ -27,8 +38,8 @@ class mcp_infractions_info
 			'title'	   => 'MCP_INFRACTIONS',
 			'version'    => '1.0.0',
 			'modes'	   => array(
-				'view'        => array('title' => 'MCP_VIEW_INFRACTIONS', 'auth' => '', 'cat' => array('MCP_INFRACTIONS')),
-				'issue'        => array('title' => 'MCP_ISSUE_INFRACTION', 'auth' => '', 'cat' => array('MCP_INFRACTIONS')),
+				'view'        => array('title' => 'MCP_VIEW_INFRACTIONS', 'auth' => 'acl_m_infractions_view', 'cat' => array('MCP_INFRACTIONS')),
+				'issue'        => array('title' => 'MCP_ISSUE_INFRACTION', 'auth' => 'acl_m_infractions_issue', 'cat' => array('MCP_INFRACTIONS')),
 			),
 		);
 	   
